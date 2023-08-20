1c69df72be613675e77606eb3397884f85c10d49
Simge Mede <109466550+simgemede@users.noreply.github.com>
2023-08-20 16:52:31 +0300

    takephoto

---
+++ 14.TakePhoto/main.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import cv2
+import uuid # Fotoya rastgele isim için gerekli kütüphane
+
+def takePhoto():
+    ret, frame = video_capture.read()
+    if ret:
+        fileName = str(uuid.uuid4()) + ".png"  # Rastgele isim oluştur
+        cv2.imwrite(fileName, frame)
+        print(f"Fotoğraf kaydedildi: {fileName}")
+
+def exitApp():
+    video_capture.release()
+    window.quit()
+
+window = tk.Tk()
+window.title("FotoÇek")
+window.geometry("400x300")
+window.resizable(False, False)
+
+video_source = 0
+video_capture = cv2.VideoCapture(video_source)
+
+frame = tk.Frame(window)
+frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+btn_snapshot = tk.Button(frame, text="Fotoğraf Çek", width=20, command=takePhoto)
+btn_snapshot.pack(padx=20, pady=10)
+
+btn_exit = tk.Button(frame, text="Çıkış", width=20, command=exitApp)
+btn_exit.pack(padx=20, pady=10)
+
+window.mainloop()
